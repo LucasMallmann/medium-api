69c3d1b285f57430cece1cf5811e2850cd66de9e
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-06-21 14:30:58 -0300

    Modify db config for heroku postgres

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 'use strict'
 
-/** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
-
-/** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
@@ -32,7 +32,9 @@ module.exports = {
   sqlite: {
     client: 'sqlite3',
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get('DB_DATABASE', 'development')}.sqlite`
+      )
     },
     useNullAsDefault: true,
     debug: Env.get('DB_DEBUG', false)
@@ -73,11 +75,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   }
